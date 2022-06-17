efbf83ff7f59a031d3d3905c1f7fcaf0254457c8
Grace Kostanich <fl5812597@gmail.com>
2022-06-17 16:54:09 -0700

    WIP: Bugfix function to output users chosen toppings and pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function Pizza(toppings, size){
 //let pizzaOrder = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
 
 let pizzaOrder = new Pizza(["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], ["Regular", "Large"]);
+console.log(pizzaOrder);
 let pizzaOrderArray = [pizzaOrder]
 console.log(pizzaOrderArray);
 
@@ -14,7 +15,7 @@ function userOrder(toppings, size, pizzaOrderArray) {
   console.log(emptyArray);
 
   pizzaOrderArray.forEach(function(){
-    if (pizzaOrderArray.toppings[i] === toppings && pizzaOrderArray.size[i] === size) {
+    if (pizzaOrderArray.toppings === toppings || pizzaOrderArray.size === size) {
       emptyArray.push(toppings + " " + size);
       console.log(toppings);
     }
@@ -94,4 +95,81 @@ function checkOrderDetails(userToppings, userSize, orderArray){
 }
 
 checkOrderDetails("Extra Cheese", "Regular", orderArray);
+//--------------------------------------------------------------------------------------------
 
+
+
+
+function Pizza(toppings, sizes){
+  this.toppings = toppings;
+  this.sizes = sizes;
+}
+
+//define variables/objects for pizza toppings 
+let extraCheese = { toppings: "Extra Cheese" };
+let pepperoni = {toppings: "Pepperoni"};
+let blackOlives = {toppings: "Black Olives"};
+let mushrooms = {toppings: "Mushrooms"};
+let bellPeppers = {topping: "Bell Peppers"};
+let bacon = {topping: "Bacon"};
+let sausage = {topping: "Sausage"};
+let regular = {sizes: "Regular"};
+let large = {sizes: "Large"};
+
+let wholeOrder = {
+  toppings: [extraCheese, pepperoni, blackOlives, mushrooms, bellPeppers, bacon, sausage],
+  sizes: [regular, large]
+};
+
+//wholeOrder.toppings;
+
+function checkOrder(userTopping, wholeOrder) {
+  emptyArray = []
+
+  wholeOrder.toppings.forEach(function(theTopping) {
+    console.log(theTopping);
+
+    if (theTopping.topping === userTopping) {
+      console.log(userTopping);
+    }
+  });
+
+}
+
+checkOrder(mushrooms, wholeOrder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// orderArray = [allToppings, allSizes]
+// console.log(orderArray)
+
+// function checkOrderDetails(userToppings, userSize, orderArray){
+//     emptyArray = []
+//     orderArray.forEach(function(bothObjects){
+//       if (bothObjects.allToppings === userToppings || bothObjects.allSizes === userSize){
+//         emptyArray.push("test");
+//     }
+//   });
+//   return emptyArray;
+// }
+
+// checkOrderDetails("Extra Cheese", "Regular", orderArray);
