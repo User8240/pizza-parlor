9187ef65f2dfc63bb415ea1949bbec93075ef187
Grace Kostanich <fl5812597@gmail.com>
2022-06-17 13:20:28 -0700

    Add more toppings to list | Correct size object

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ function Pizza(toppings, size){
   this.toppings = toppings;
   this.size = size;
 }
-// let myPizza = new Pizza("Cheese", "Salad");
+// let myPizza = new Pizza("Extra Cheese", "Regular");
 // myPizza;
 // let extraCheese = {toppings: "Extra Cheese"};
 // let Peperoni = {toppings: "Pepperoni"};
-let allToppings = {toppings: ["extraCheese", "Peperoni"], size: ["Salad", "Breadsticks", "Sugar Bites"]};
+let allToppings = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
 // allToppings;
 // allToppings.toppings;
 // allToppings.size;
