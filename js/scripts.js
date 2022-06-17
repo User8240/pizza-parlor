b4add3c46dcfe96cf6f5ff62d22c8e2f8f210c8d
Grace Kostanich <fl5812597@gmail.com>
2022-06-17 14:52:24 -0700

    WIP: Create a function that outputs users toppings and size into an array

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,23 @@ function Pizza(toppings, size){
   this.size = size;
 }
 
-let pizzaOrder = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
+//let pizzaOrder = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
+
+let pizzaOrder = new Pizza(["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], ["Regular", "Large"]);
 let pizzaOrderArray = [pizzaOrder]
+console.log(pizzaOrderArray);
 
 function userOrder(toppings, size, pizzaOrderArray) {
-  emptyArray = []
-  pizzaOrderArray.forEach(function(order){
-    if (pizzaOrderArray.toppings === toppings && pizzaOrderArray.size === size) {
-      emptyArray.push(order)
+  emptyArray = [];
+  console.log(emptyArray);
+
+  pizzaOrderArray.forEach(function(){
+    if (pizzaOrderArray.toppings[i] === toppings && pizzaOrderArray.size[i] === size) {
+      emptyArray.push(toppings + " " + size);
+      console.log(toppings);
     }
   });
   return emptyArray;
 }
+userOrder("Extra Cheese", "Regular", pizzaOrderArray);
+console.log(emptyArray);
