fec8128346485c75221d132622ba9998448838dd
Grace Kostanich <fl5812597@gmail.com>
2022-06-17 12:09:50 -0700

    Add passing test | Add list of toppings and sides as the key value for each key (toppings, sides)

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,13 @@ function Pizza(toppings, sides){
   this.toppings = toppings;
   this.sides = sides;
 }
-let myPizza = new Pizza("Cheese", "Salad");
-myPizza;
+// let myPizza = new Pizza("Cheese", "Salad");
+// myPizza;
+// let extraCheese = {toppings: "Extra Cheese"};
+// let Peperoni = {toppings: "Pepperoni"};
+
+let allToppings = {toppings: ["extraCheese", "Peperoni"], sides: ["Salad", "Breadsticks", "Sugar Bites"]};
+
+allToppings;
+allToppings.toppings;
+allToppings.sides;
