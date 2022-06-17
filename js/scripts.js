b1405bcc8217a5f5bef3acf84c1bcae2e4855a2c
Grace Kostanich <fl5812597@gmail.com>
2022-06-17 13:54:39 -0700

    WIP: Create a function to output array of the users order (toppings & size)

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ function Pizza(toppings, size){
   this.toppings = toppings;
   this.size = size;
 }
-// let myPizza = new Pizza("Extra Cheese", "Regular");
-// myPizza;
-// let extraCheese = {toppings: "Extra Cheese"};
-// let Peperoni = {toppings: "Pepperoni"};
-let allToppings = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
-// allToppings;
-// allToppings.toppings;
-// allToppings.size;
+
+let pizzaOrder = {toppings: ["Extra Cheese", "Pepperoni", "Black Olives", "Mushrooms", "Bell Peppers", "Bacon", "Sausage"], size: ["Regular", "Large"]};
+let pizzaOrderArray = [pizzaOrder]
+
+function userOrder(toppings, size, pizzaOrderArray) {
+  emptyArray = []
+  pizzaOrderArray.forEach(function(order){
+    if (pizzaOrderArray.toppings === toppings && pizzaOrderArray.size === size) {
+      emptyArray.push(order)
+    }
+  });
+  return emptyArray;
+}
