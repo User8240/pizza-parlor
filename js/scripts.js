cc8d84e7917985321f0d0920ccc3184ba58e0d7f
Grace Kostanich <fl5812597@gmail.com>
2022-06-18 02:25:37 -0700

    WIP: Bugfix checkOrder function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function Pizza(toppings, sizes){
   this.toppings = toppings;
   this.sizes = sizes;
-}
+};
 
 //define variables/objects for pizza toppings 
+let none = { toppings: "None" };
 let extraCheese = { toppings: "Extra Cheese" };
 let pepperoni = {toppings: "Pepperoni"};
 let blackOlives = {toppings: "Black Olives"};
@@ -15,74 +16,60 @@ let regular = {sizes: "Regular"};
 let large = {sizes: "Large"};
 
 let wholeOrder = {
-  toppings: [extraCheese, pepperoni, blackOlives, mushrooms, bellPeppers, bacon, sausage],
+  toppings: [none, extraCheese, pepperoni, blackOlives, mushrooms, bellPeppers, bacon, sausage],
   sizes: [regular, large]
 };
 
-//wholeOrder.toppings;
+// Pizza.prototype.price = function(priceArray){
+//   this.pizzaTotal = 0
+//   if (priceArray[i] === "Regular") {
+//     pizzaTotal += 8 
+//   } else {
+//     pizzaTotal += 12
+//   }
+//   return pizzaTotal
+// };
 
 function checkOrder(userTopping, userSize, wholeOrder) {
   emptyArray = [];
+  priceArray = [] 
 
   wholeOrder.toppings.forEach(function(theTopping) {
     console.log(theTopping);
 
     if (theTopping.toppings === userTopping) {
-      alert("it worked");
-      console.log(theTopping.toppings);
-      emptyArray.push(theTopping.toppings)
-    } 
+      //console.log(theTopping.toppings);
+      emptyArray.push(theTopping.toppings);
+      console.log(userTopping);
+    }; 
   });
     
   wholeOrder.sizes.forEach(function(theSize) {
     console.log(theSize);
 
     if (theSize.sizes === userSize) {
-      alert("it worked");
-      console.log(theSize.sizes);
-      emptyArray.push(theSize.sizes)
-    }; 
+      //console.log(theSize.sizes);
+      priceArray.push(theSize.sizes);
+     }; 
   });
 return emptyArray 
 };
 
 checkOrder("Mushrooms", "Regular", wholeOrder);
 console.log(emptyArray);
-
-//emptyArray.push
-
+console.log(priceArray);
 
 
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     let none = $("#none").val();
+//     let userOutput = checkOrder(none);
+//     $(".userOutput").text(userOutput);
+//     console.log(userOutput);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// orderArray = [allToppings, allSizes]
-// console.log(orderArray)
-
-// function checkOrderDetails(userToppings, userSize, orderArray){
-//     emptyArray = []
-//     orderArray.forEach(function(bothObjects){
-//       if (bothObjects.allToppings === userToppings || bothObjects.allSizes === userSize){
-//         emptyArray.push("test");
-//     }
+//     $("#show-order").show();
 //   });
-//   return emptyArray;
-// }
+// });
 
-// checkOrderDetails("Extra Cheese", "Regular", orderArray);
